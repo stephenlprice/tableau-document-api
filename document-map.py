5f7d61a6fd983e18bb7ef816fde5b65b9cb5c03b
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2022-01-23 23:43:16 -0600

    tabulating datasources

--- document-map.py
+++ document-map.py
@@ -1,36 +1,39 @@
 from tableaudocumentapi import Workbook
 from tabulate import tabulate
 
+# describe the workbook file and object
 sourceWB = Workbook('./assets/workbooks/Superstore.twbx')
+wbPath = sourceWB.filename
+wbName = sourceWB.filename.rsplit('/', 1)[-1]
 
-print('\n', sourceWB.filename, '\n')
-print('type:', type(sourceWB), '\n', sourceWB)
-print('\n --------------- \n')
+print(f'\n *** Analyzing Tableau workbook {wbName} ***\n')
+print(f'The file is located at {wbPath}')
+print(f'The workbook object is of type: {type(sourceWB)} \n')
 
+# describe datasources
 datasources = sourceWB.datasources
-
-print('datasources', 'type:', type(datasources), '\n')
+dsList = []
 for datasource in datasources:
-  print('datasource:', datasource)
-print('\n --------------- \n')
+  # tabulate() requires a list of lists containing the object typed as a string as it cannot iterate over class objects
+  dsList.append([str(datasource)])
 
-print(datasource.connections)
+print(f'There are {len(datasources)} datasources in this workbook')
+print(f'The datasources object is of type: {type(datasources)}')
+print(tabulate(dsList, headers=['Datasources'], showindex=True, tablefmt='fancy_grid'), '\n')
 
+
+# describe datasource connections
 for datasource in datasources:
-  print(datasource.connections)
-  print('\n --------------- \n')
-  for connection in datasource.connections:
-    print('connection', 'type:', type(connection), '\n', connection)
-  print('\n ---------------')
-print('\n --------------- \n')
+  if (len(datasource.connections) > 0):
+    print(datasource.connections)
+    print('\n --------------- \n')
+    for connection in datasource.connections:
+      table = {
+        'Property': ['server', 'dbname', 'username', 'dbclass', 'port', 'query_band', 'initial_sql'],
+        'Value': [connection.server, connection.dbname, connection.username, connection.dbclass, connection.port, connection.query_band, connection.initial_sql]
+      }
+      
+      print('connection', 'type:', type(connection), '\n', connection)
+      print(tabulate(table, headers='keys', showindex=True, tablefmt='fancy_grid'))
 
-# print('datasources ', 'type: ', type(datasources), '\n', tabulate(datasources), '\n \n --------------- \n')
-
-
-
-
-
-
-
-# for connections in datasources:
-#   print('connections: ', connections) 
+    print('\n ---------------')
