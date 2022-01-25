549f7d56eb03f6403f93e7f021b2f17c53ac3313
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2022-01-25 00:52:45 -0600

    displaying a table listing datasources and connections

--- document-map.py
+++ document-map.py
@@ -12,30 +12,38 @@ print(f'The workbook object is of type: {type(sourceWB)} \n')
 
 # describe datasources
 datasources = sourceWB.datasources
-dsList = {}
-for index, datasource in enumerate(datasources):
-  # tabulate() requires a list of lists containing the object typed as a string as it cannot iterate over class objects
-  dsList.index = str(datasource)
-  # for connection in datasource.connections:
+dsList = []
+for datasource in datasources:
+  ds = str(datasource) # datasource class object typed to a string
+  conn = [] # list of connections
+  if len(datasource.connections) > 0:
+    for connection in datasource.connections:
+      if connection:
+        conn.append(connection.dbclass) # not every datasource has a name but it will have connection information
+      else:
+        conn.append('none')
+  else:
+    conn.append('none')
+
+  dsList.append([ds, conn]) # list all datasources along with a list of corresponding connections
 
 
-print(f'There are {len(datasources)} datasources in this workbook')
+print(f' *** There are {len(datasources)} datasources in this workbook ***')
 print(f'The datasources object is of type: {type(datasources)}')
 print(tabulate(dsList, headers=['Datasources', 'Connections'], showindex=True, tablefmt='fancy_grid'), '\n')
 
 
 # describe datasource connections
+print('******* Connections ******')
 for datasource in datasources:
-  if (len(datasource.connections) > 0):
-    print(datasource.connections)
-    print('\n --------------- \n')
-    for connection in datasource.connections:
-      table = {
-        'Property': ['server', 'dbname', 'username', 'dbclass', 'port', 'query_band', 'initial_sql'],
-        'Value': [connection.server, connection.dbname, connection.username, connection.dbclass, connection.port, connection.query_band, connection.initial_sql]
-      }
-      
-      print('connection', 'type:', type(connection), '\n', connection)
-      print(tabulate(table, headers='keys', showindex=True, tablefmt='fancy_grid'))
+  print(f'***** Datasource: {datasource} length: {len(datasource.connections)} *******')
+  for connection in datasource.connections:
+    connList = {
+      'Property': ['server', 'dbname', 'username', 'dbclass', 'port', 'query_band', 'initial_sql'],
+      'Value': [connection.server, connection.dbname, connection.username, connection.dbclass, connection.port, connection.query_band, connection.initial_sql]
+    }
+    print('******* Each Connection ******')
+    print('connection', 'type:', type(connection), '\n', connection)
+    print(tabulate(connList, headers='keys', showindex=True, tablefmt='fancy_grid'))
 
-    print('\n ---------------')
+  print('\n ---------------')
